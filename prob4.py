8d5874b9cab0e966198f3a92e8bb766674160421
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-06-24 16:34:52 -0700

    solved prob 4

---
+++ prob4.py
@@ -0,0 +1,9 @@
+def num_within(a,b,c):
+    if a in range(x,y) and b in range(x,y) and c in range(x,y):
+        return True
+    else:
+        return False
+
+x = 1
+y = 12
+print(num_within(4, 4, 45))
